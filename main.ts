3aa363516532fa052b1f9081bf1a2a6ed526234d
bwendt-unc <68039945+bwendt-unc@users.noreply.github.com>
2020-07-23 13:36:19 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,9 +3,11 @@ let x = 2
 led.plot(x, y)
 basic.forever(function () {
     while (y > 0) {
+        basic.pause(1000)
         led.plot(0, 0)
     }
     while (y < 4) {
+        basic.pause(1000)
         led.plot(0, 0)
     }
 })
